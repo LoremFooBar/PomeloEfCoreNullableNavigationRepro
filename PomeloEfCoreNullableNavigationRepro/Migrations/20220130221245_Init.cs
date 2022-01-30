cc7c24d2bd1ea45627a5d5918f2ef9191cd7a47b
Amit Edelshtein <amit@camilyo.com>
2022-01-30 17:18:57 -0500

    Initial commit

---
+++ PomeloEfCoreNullableNavigationRepro/Migrations/20220130221245_Init.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PomeloEfCoreNullableNavigationRepro.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Maker = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Model = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Year = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CarId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_People_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_CarId",
+                table: "People",
+                column: "CarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ PomeloEfCoreNullableNavigationRepro/TestDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PomeloEfCoreNullableNavigationRepro;
+
+public class TestDbContext : DbContext
+{
+    public DbSet<Person> People => Set<Person>();
+    public DbSet<Car> Cars => Set<Car>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionString = "server=localhost;database=test1;User ID=root;password=Lmot5q";
+        optionsBuilder.UseMySql(connectionString, MariaDbServerVersion.LatestSupportedServerVersion);
+    }
+}
+
+[Serializable]
+public class Person
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public int Age { get; set; }
+    public Car Car { get; set; } = null!;
+}
+
+[Serializable]
+public class Car
+{
+    public int Id { get; set; }
+    public string Maker { get; set; } = null!;
+    public string Model { get; set; } = null!;
+    public int Year { get; set; }
+}
